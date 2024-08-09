eb42a7261ed4e289d14d66affd22925845d23fd7
Aleksandr Samokhin <beetee.tanais@yandex.ru>
2024-08-09 11:49:27 +0300

    fix : исправление замечаний согласно ревью fix: fix imports

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,25 +1,16 @@
 package ru.practicum.shareit.item.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
-/**
- * TODO Sprint add-controllers.
- */
 
 @Data
 @Builder
 @AllArgsConstructor
 public class Item {
     private int id;
-    @NotBlank
     private String name;
-    @NotBlank
     private String description;
-    @NotNull
     private Boolean available;
     private Integer owner;
     private Integer request;
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.user.model;
 
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
