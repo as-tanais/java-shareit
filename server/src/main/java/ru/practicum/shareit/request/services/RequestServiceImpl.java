b6ccf405ec3787bfd290a1f78df601d7ba7d5882
Tanais <beetee.tanais@yandex.ru>
2024-08-25 23:02:33 +0300

    fix fix fix

--- server/src/main/java/ru/practicum/shareit/request/services/RequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/services/RequestServiceImpl.java
@@ -7,7 +7,7 @@ import ru.practicum.shareit.item.mappers.ItemMapper;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.repository.ItemRepository;
 
-import ru.practicum.shareit.request.ItemRequestMapper;
+import ru.practicum.shareit.request.mappers.ItemRequestMapper;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
